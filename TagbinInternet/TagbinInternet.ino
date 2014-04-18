ea80261829025256ac04edfa316b048947222563
Chetanbansiwal <chetanbansiwal@live.co.uk>
2014-04-18 17:11:14 +0530

    first edit Arduino Internet

--- TagbinInternet/TagbinInternet.ino
+++ TagbinInternet/TagbinInternet.ino
@@ -8,7 +8,21 @@ digitalWrite(21,LOW);
 delay(3000);
 digitalWrite(21,HIGH);
 delay(1000);
-Serial1.print("at+netmode=?\r\n");
+Serial1.print("at+netmode=2\r\n");
+Serial1.print("at+wifi_conf=Tagbin,wpawpa2_tkip,!@tagbin12\r\n");
+Serial1.print("at+dhcpc=1\r\n");
+//Serial1.print("at+remoteip=192.168.1.9\r\n");
+//Serial1.print("at+remoteport=8080\r\n");
+Serial1.print("at+remotepro=tcp\r\n");
+Serial1.print("at+timeout=0\r\n");
+Serial1.print("at+mode=server\r\n");
+Serial1.print("at+uart=115200,8,n,1\r\n");
+Serial1.print("at+uartpacklen=64\r\n");
+Serial1.print("at+uartpacktimeout=10\r\n");
+delay(500);
+Serial1.print("at+net_commit=1\r\n");
+delay(500);
+Serial1.print("at+reconn=1\r\n");
 }
 
 void loop() {
@@ -16,5 +30,9 @@ void loop() {
   if (Serial1.available()) {
     int inByte = Serial1.read();
     Serial.write(inByte);
+  }
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
   }
 }
