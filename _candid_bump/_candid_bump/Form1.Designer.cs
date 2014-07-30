f3b271fb7724fa1fa1e5ef1bcb1813ce2bb1bcef
Ankit <contact_ankit611@yahoo.com>
2014-07-30 16:38:56 +0530

    candid multiscreen added

---
+++ _candid_bump/_candid_bump/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace _candid_bump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin22 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin23 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin24 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin25 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin26 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin27 = new AxAXVLC.AxVLCPlugin2();
+            this.axVLCPlugin28 = new AxAXVLC.AxVLCPlugin2();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin24)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin25)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin26)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin27)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin28)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 12);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(145, 115);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // axVLCPlugin22
+            // 
+            this.axVLCPlugin22.Enabled = true;
+            this.axVLCPlugin22.Location = new System.Drawing.Point(192, 12);
+            this.axVLCPlugin22.Name = "axVLCPlugin22";
+            this.axVLCPlugin22.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin22.OcxState")));
+            this.axVLCPlugin22.Size = new System.Drawing.Size(144, 115);
+            this.axVLCPlugin22.TabIndex = 1;
+            // 
+            // axVLCPlugin23
+            // 
+            this.axVLCPlugin23.Enabled = true;
+            this.axVLCPlugin23.Location = new System.Drawing.Point(377, 12);
+            this.axVLCPlugin23.Name = "axVLCPlugin23";
+            this.axVLCPlugin23.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin23.OcxState")));
+            this.axVLCPlugin23.Size = new System.Drawing.Size(130, 115);
+            this.axVLCPlugin23.TabIndex = 2;
+            // 
+            // axVLCPlugin24
+            // 
+            this.axVLCPlugin24.Enabled = true;
+            this.axVLCPlugin24.Location = new System.Drawing.Point(538, 11);
+            this.axVLCPlugin24.Name = "axVLCPlugin24";
+            this.axVLCPlugin24.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin24.OcxState")));
+            this.axVLCPlugin24.Size = new System.Drawing.Size(135, 115);
+            this.axVLCPlugin24.TabIndex = 3;
+            // 
+            // axVLCPlugin25
+            // 
+            this.axVLCPlugin25.Enabled = true;
+            this.axVLCPlugin25.Location = new System.Drawing.Point(12, 150);
+            this.axVLCPlugin25.Name = "axVLCPlugin25";
+            this.axVLCPlugin25.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin25.OcxState")));
+            this.axVLCPlugin25.Size = new System.Drawing.Size(137, 113);
+            this.axVLCPlugin25.TabIndex = 4;
+            // 
+            // axVLCPlugin26
+            // 
+            this.axVLCPlugin26.Enabled = true;
+            this.axVLCPlugin26.Location = new System.Drawing.Point(192, 150);
+            this.axVLCPlugin26.Name = "axVLCPlugin26";
+            this.axVLCPlugin26.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin26.OcxState")));
+            this.axVLCPlugin26.Size = new System.Drawing.Size(137, 104);
+            this.axVLCPlugin26.TabIndex = 5;
+            // 
+            // axVLCPlugin27
+            // 
+            this.axVLCPlugin27.Enabled = true;
+            this.axVLCPlugin27.Location = new System.Drawing.Point(377, 150);
+            this.axVLCPlugin27.Name = "axVLCPlugin27";
+            this.axVLCPlugin27.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin27.OcxState")));
+            this.axVLCPlugin27.Size = new System.Drawing.Size(130, 100);
+            this.axVLCPlugin27.TabIndex = 6;
+            // 
+            // axVLCPlugin28
+            // 
+            this.axVLCPlugin28.Enabled = true;
+            this.axVLCPlugin28.Location = new System.Drawing.Point(538, 150);
+            this.axVLCPlugin28.Name = "axVLCPlugin28";
+            this.axVLCPlugin28.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin28.OcxState")));
+            this.axVLCPlugin28.Size = new System.Drawing.Size(135, 100);
+            this.axVLCPlugin28.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 294);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axVLCPlugin28);
+            this.Controls.Add(this.axVLCPlugin27);
+            this.Controls.Add(this.axVLCPlugin26);
+            this.Controls.Add(this.axVLCPlugin25);
+            this.Controls.Add(this.axVLCPlugin24);
+            this.Controls.Add(this.axVLCPlugin23);
+            this.Controls.Add(this.axVLCPlugin22);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin24)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin25)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin26)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin27)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin28)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin22;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin23;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin24;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin25;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin26;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin27;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin28;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ _candid_bump/_candid_bump/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using AxAXVLC;
+using AXVLC;
+using System.Threading;
+
+
+
+namespace _candid_bump
+{
+    public partial class Form1 : Form
+    {
+
+        static SerialPort serial;
+        String temp = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            AxVLCPlugin2 vlc1 = new AxVLCPlugin2();
+            form.Controls.Add(vlc1);
+            form.Show();
+
+
+            Thread thread = new Thread(new ThreadStart(init));
+            thread.Start();
+            
+          
+        }
+
+        public void init()
+        {
+
+            //INITIALIZE VLC
+            var uri = new Uri(@"F:\videos\SEASONS\How I Met Your Mother\Season 1-5 Bloopers.FLV");
+            var convertedURI = uri.AbsoluteUri;
+            axVLCPlugin21.playlist.add(convertedURI);
+            axVLCPlugin22.playlist.add(convertedURI);
+            axVLCPlugin23.playlist.add(convertedURI);
+            axVLCPlugin24.playlist.add(convertedURI);
+            axVLCPlugin25.playlist.add(convertedURI);
+            axVLCPlugin26.playlist.add(convertedURI);
+            axVLCPlugin27.playlist.add(convertedURI);
+            axVLCPlugin28.playlist.add(convertedURI); 
+            
+           
+ 
+            serial = new SerialPort("COM32", 9600, Parity.None, 8, StopBits.One)
+            {
+                Handshake = Handshake.None
+            };
+
+            serial.DataReceived += SerialPortdataReceived;
+            //serial.Open();
+            
+        }
+
+        private void SerialPortdataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string tempData = sp.ReadLine();
+
+           
+
+            String[] indataArray = tempData.Split('@');
+            
+            foreach(String indata in indataArray)
+            {
+
+                if (temp != indata && indata.Length == 5)
+                {
+                    //NEW DATA
+                    Console.Write(indata+ "  ");
+                    temp = indata;
+
+                    String[] _indata = indata.Split('_'); // 1_HIG  OR  1_LOW
+
+                    if (_indata[1] == "HIG")
+                    {
+                        Console.WriteLine("Playing Screen " + _indata[0]);
+                        //axVLCPlugin21.playlist.play();
+                        play(returnPluginScreen(_indata[0]));
+                    }
+                    if (_indata[1] == "LOW")
+                    {
+                        Console.WriteLine("Stopping Screen " + _indata[0]);
+                        //stop(returnPluginScreen(_indata[0]));
+                    }
+                
+                } 
+
+            }
+            Array.Clear(indataArray, 0, indataArray.Length);
+            Console.WriteLine("/===============================/");
+             
+            
+            
+      
+        }
+
+        public void play(AxVLCPlugin2 screen)
+        {
+            if( !screen.playlist.isPlaying )
+                screen.playlist.play();
+            
+
+        }
+
+       
+
+
+        public void stop(AxVLCPlugin2 screen)
+        {
+            if (screen.playlist.isPlaying)
+                screen.playlist.stop();
+
+        }
+
+        public AxVLCPlugin2 returnPluginScreen(String screen)
+        {
+            switch (screen)
+            {
+                case "1":
+                    return axVLCPlugin21;
+                case "2":
+                    return axVLCPlugin22;
+                case "3":
+                    return axVLCPlugin23;
+                case "4":
+                    return axVLCPlugin24;
+                case "5":
+                    return axVLCPlugin25;
+                case "6":
+                    return axVLCPlugin26;
+                case "7":
+                    return axVLCPlugin27;
+                case "8":
+                    return axVLCPlugin28;
+                default :
+                    return axVLCPlugin21;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Play clicked");
+            //INITIALIZE VLC
+            axVLCPlugin21.playlist.play();
+        }
+
+       
+    }
+}
